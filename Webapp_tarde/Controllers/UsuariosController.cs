6c53bedd03b87fe1a8e5351dedb3525516798647
0220482212021 <0220482212021@LAB1MAQ22>
2023-03-22 16:17:07 -0300

    Add project files.

---
+++ Webapp_tarde/Controllers/UsuariosController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Webapp_tarde.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+    }
+}
